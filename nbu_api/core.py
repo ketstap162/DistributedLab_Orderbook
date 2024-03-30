d9041757c1edfceca2232201cb6e28e5fb84ba18
ketstap162 <zp162162@gmail.com>
2024-03-30 02:02:18 +0200

    Implement NBU API tool

--- nbu_api/core.py
+++ nbu_api/core.py
@@ -1,6 +1,27 @@
+import os
+
+import requests
+
+NBU_API_PATH = os.environ.get("NBU_API_URL", "https://bank.gov.ua/NBUStatService/v1/")
+# https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&valcode=usd
 
 
+class NBU:
+    path = NBU_API_PATH
+
+    @classmethod
+    def get_currency_data(cls, currency_code):
+        endpoint_path = cls.path + "statdirectory/exchange"
+        response = requests.get(endpoint_path, params={
+            "json": "",
+            "valcode": currency_code,
+        })
+
+        return response.json()[0]
 
 
 if __name__ == "__main__":
     print("National bank of Ukraine")
+    data = NBU.get_currency_data("usd")
+    print("Type", type(data))
+    print(data)
