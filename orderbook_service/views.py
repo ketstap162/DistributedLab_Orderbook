44ef8635f8132fe06edec6ff7a3a234132673534
ketstap162 <zp162162@gmail.com>
2024-04-18 17:25:24 +0300

    Removing the excess

--- orderbook_service/views.py
+++ orderbook_service/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
 
 from nbu_api.core import NBU
-from trading_app.models import Currency
-
-
-def get_all_currencies_rate(base_currency, currencies) -> dict:
-    rates = {}
-    for currency in currencies:
-        rate = NBU.get_currency_rate(currency.code)
-        rates[currency.name] = f"{rate} {base_currency.name}"
-
-    return rates
 
 
 def index_view(request):
